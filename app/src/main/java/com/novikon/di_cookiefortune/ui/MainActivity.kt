878d28d3e44d10cf9d1d3e461b176c9bb8977649
judithcarballido <judithcarballido@gmail.com>
2025-12-01 17:35:54 +0100

    Terminar fase 2 (- arrays)

---
+++ app/src/main/java/com/novikon/di_cookiefortune/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.novikon.di_cookiefortune.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.novikon.di_cookiefortune.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val imatgeCookie =  findViewById<ImageView>(R.id.imageCookie)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        imatgeCookie.setOnClickListener {
+            val intent = Intent(this, PhraseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+
+}
--- app/src/main/java/com/novikon/di_cookiefortune/ui/PhraseActivity.kt
+++ app/src/main/java/com/novikon/di_cookiefortune/ui/PhraseActivity.kt
@@ -1,17 +1,19 @@
-package com.novikon.di_cookiefortune
+package com.novikon.di_cookiefortune.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.novikon.di_cookiefortune.R
 
-class MainActivity : AppCompatActivity() {
+class PhraseActivity : AppCompatActivity() {  // <- AGREGAR : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        setContentView(R.layout.activity_phrase)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.phrase)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
